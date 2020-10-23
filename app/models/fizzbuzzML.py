ddd080d73a5c7fc327fff2199399932dd6cbae6b
phenixzr <phoenixzr@hotmail.fr>
2020-10-23 23:17:53 +0200

    Adding in db requests

--- app/models/fizzbuzzML.py
+++ app/models/fizzbuzzML.py
@@ -1,4 +1,25 @@
 from marshmallow import Schema, fields
+from app.extensions import db
+
+class FizzBuzzDb(db.Model):
+    __tablename__ = 'fizzbuzz'
+
+    id = db.Column(db.Integer, primary_key=True)
+    int1 = db.Column(db.Integer())
+    int2 = db.Column(db.Integer())
+    limit = db.Column(db.Integer())
+    str1 = db.Column(db.String())
+    str2 = db.Column(db.String())
+
+    def __init__(self, int1, int2, limit, str1, str2):
+        self.int1 = int1
+        self.int2 = int2
+        self.limit = limit
+        self.str1 = str1
+        self.str2 = str2
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
 
 class FizzBuzzMl():
     def __init__(self, int1, int2, limit, str1, str2):
--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from flask import Flask
 from flask_restful import Resource, Api
-from flask_sqlalchemy import SQLAlchemy
 from app.endpoints.fizzbuzzEP import FizzBuzzEP
 from app.endpoints.metricsEP import MetricsEP
+from app.extensions import db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://fzapiuser:jn45vrt5v23@localhost/fizzbuzz'
@@ -11,6 +11,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app)
 api.add_resource(FizzBuzzEP, '/v1/fizzbuzz')
 api.add_resource(MetricsEP, '/v1/metrics')
+
+db.init_app(app)
 
 if __name__ == '__main__':
     app.run()  # run our Flask app
